9225547d34744b6db37542ddc2d3626a7cf0630e
Paul <contact@p-soulhiard.fr>
2020-02-21 18:58:30 +0100

    last

--- src/main.js
+++ src/main.js
@@ -1,42 +1,56 @@
 // import barba from '@barba/core'
-// import gsap from 'gsap'
+import gsap from 'gsap'
 
 
 const colorMode = document.querySelector('.mode-switcher')
 const body = document.querySelector('body')
 const images = document.querySelectorAll('.images')
+const modal = document.querySelector('.pop-up')
 let scrollY;
 let page;
 
-document.querySelector('#email').addEventListener('click', function(){
-  document.querySelector('.pop-up').classList.add('flex')
-  console.log(y)
-})
-
-document.querySelector('.p-mail .close').addEventListener('click', function(){
-  document.querySelector('.pop-up').classList.remove('flex')
-})
-
 window.addEventListener('scroll', function(){
-  y = window.scrollY;
+  scrollY = window.scrollY;
 })
+
+
+
+function modalStart() {
+  document.querySelector('#email').addEventListener('click', function(){
+    document.querySelector('.pop-up').classList.add('flex')
+    body.classList.add('modal-open')
+    const tl = gsap.timeline()
+    tl.fromTo('.pop-up', {autoAlpha:0}, {autoAlpha:1, duration:0.6, ease:'power3.out'})
+    .fromTo('.p-mail', {autoAlpha:0, y:40}, {autoAlpha:1, y:0, duration:0.6, ease:'power3.out'}, "-=0.4")
+  })
+
+  document.querySelector('.p-mail .close').addEventListener('click', function(){
+    const tl = gsap.timeline()
+    const done = () => {
+      document.querySelector('.pop-up').classList.remove('flex')
+    }
+    body.classList.remove('modal-open')
+    tl.fromTo('.pop-up', {autoAlpha:1}, {autoAlpha:0, duration:0.6, ease:'power3.out'})
+    .fromTo('.p-mail', {autoAlpha:1, y:0}, {autoAlpha:0, y:40, duration:0.6, ease:'power3.out', onComplete : done}, "-=0.6")
+  })
+}
+modalStart()
 
 function sliderOpacity() {
   const target = document.querySelector('.c-slider')
   const targetHeight = target.clientHeight
+  gsap.set(target, {autoAlpha:'100%'})
 
   window.addEventListener('scroll', function(){
-    const scrollPercent = Math.floor((y / targetHeight) * 100)
+    const scrollPercent = Math.floor((scrollY / targetHeight) * 100)
 
     if(scrollPercent < 100) {
-      console.log(scrollPercent)
-      target.style.opacity = 100 - (scrollPercent * 1.65) + '%';
+      gsap.set(target, {autoAlpha: 100 - (scrollPercent * 1.65) + '%'})
     } else {
-      return;
+      return
     }
   })
 }
-
 sliderOpacity()
 
 const prismicContent = () => {
@@ -70,13 +84,13 @@ const aboutData = [
   document.querySelector('.c-data-n3')
 ]
 
-// console.log(aboutHover)
-// aboutHover.forEach(function(item, nb) {
-//   item.addEventListener('mouseenter', function(){
-//     aboutData[nb].classList.add('active')
-//     setTimeout(function(){aboutData[nb].classList.remove('active')}, 5000)
-//   })
-// })
+console.log(aboutHover)
+aboutHover.forEach(function(item, nb) {
+  this.addEventListener('mouseenter', function(){
+    aboutData[nb].classList.add('active')
+    setTimeout(function(){aboutData[nb].classList.remove('active')}, 5000)
+  })
+})
 
 
 
