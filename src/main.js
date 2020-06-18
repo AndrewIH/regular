65522051c0961c040d09b7af6edd8b04841932bd
Paul <contact@p-soulhiard.fr>
2020-06-18 21:16:01 +0200

    v1

--- src/main.js
+++ src/main.js
@@ -13,8 +13,6 @@ window.addEventListener('scroll', function(){
   scrollY = window.scrollY;
 })
 
-
-
 function modalStart() {
   document.querySelector('#email').addEventListener('click', function(){
     document.querySelector('.pop-up').classList.add('flex')
@@ -34,7 +32,7 @@ function modalStart() {
     .fromTo('.p-mail', {autoAlpha:1, y:0}, {autoAlpha:0, y:40, duration:0.6, ease:'power3.out', onComplete : done}, "-=0.6")
   })
 }
-modalStart()
+
 
 function sliderOpacity() {
   const target = document.querySelector('.c-slider')
@@ -51,74 +49,31 @@ function sliderOpacity() {
     }
   })
 }
-sliderOpacity()
 
-const prismicContent = () => {
-  const Prismic = require('prismic-javascript');
-   
-  const apiEndpoint = "https://regular.cdn.prismic.io/api/v2";
-   
-  Prismic.getApi(apiEndpoint).then(function(api) {
-    return api.query(""); // An empty query will return all the documents
-  }).then(function(response) {
-    let page = response.results[0]
-    console.log(page.data)
-  }, function(err) {
-    console.log("Something went wrong: ", err)
-  })
+
+if (body.getAttribute('namespace') === 'home') {
+  modalStart()
+  sliderOpacity()
 }
 
-prismicContent()
+if (body.getAttribute('namespace') === 'about') {
+  const aboutHover = [
+    document.querySelector('.c-about-data-1'),
+    document.querySelector('.c-about-data-2'),
+    document.querySelector('.c-about-data-3')
+  ]
 
+  const aboutData = [
+    document.querySelector('.c-data-n1'),
+    document.querySelector('.c-data-n2'),
+    document.querySelector('.c-data-n3')
+  ]
 
-
-const aboutHover = [
-  document.querySelector('.c-about-data-1'),
-  document.querySelector('.c-about-data-2'),
-  document.querySelector('.c-about-data-3')
-]
-
-const aboutData = [
-  document.querySelector('.c-data-n1'),
-  document.querySelector('.c-data-n2'),
-  document.querySelector('.c-data-n3')
-]
-
-console.log(aboutHover)
-aboutHover.forEach(function(item, nb) {
-  this.addEventListener('mouseenter', function(){
-    aboutData[nb].classList.add('active')
-    setTimeout(function(){aboutData[nb].classList.remove('active')}, 5000)
+  console.log(aboutHover)
+  aboutHover.forEach(function(item, nb) {
+    item.addEventListener('mouseenter', function(){
+      aboutData[nb].classList.add('active')
+      setTimeout(function(){aboutData[nb].classList.remove('active')}, 5000)
+    })
   })
-})
-
-
-
-
-// barba.hooks.beforeEnter((data) => {
-//   mainLaunch()
-// })
-
-// barba.init({
-//   transitions: [{
-//     name: 'main',
-//     leave: function(data) {
-//       var done = this.async();
-//       gsap.to(data.current.container, {
-//       	duration: 0.6,
-//         opacity: 0,
-//         onComplete: done,
-//         ease: "power3.inOut"
-//       });
-//     },
-//     enter: function(data) {
-//       var done = this.async();
-//       gsap.from(data.next.container, {
-//       	duration: 0.6,
-//         opacity: 0,
-//         onComplete: done,
-//         ease: "power3.inOut"
-//       });
-//     }
-//   }]
-// })
+}
