8383c1cfe6973db67ff45537f2f956830440457f
Paul <contact@p-soulhiard.fr>
2020-06-18 22:07:41 +0200

    fix v57

--- src/main.js
+++ src/main.js
@@ -13,26 +13,6 @@ window.addEventListener('scroll', function(){
   scrollY = window.scrollY;
 })
 
-function modalStart() {
-  document.querySelector('#email').addEventListener('click', function(){
-    document.querySelector('.pop-up').classList.add('flex')
-    body.classList.add('modal-open')
-    const tl = gsap.timeline()
-    tl.fromTo('.pop-up', {autoAlpha:0}, {autoAlpha:1, duration:0.6, ease:'power3.out'})
-    .fromTo('.p-mail', {autoAlpha:0, y:40}, {autoAlpha:1, y:0, duration:0.6, ease:'power3.out'}, "-=0.4")
-  })
-
-  document.querySelector('.p-mail .close').addEventListener('click', function(){
-    const tl = gsap.timeline()
-    const done = () => {
-      document.querySelector('.pop-up').classList.remove('flex')
-    }
-    body.classList.remove('modal-open')
-    tl.fromTo('.pop-up', {autoAlpha:1}, {autoAlpha:0, duration:0.6, ease:'power3.out'})
-    .fromTo('.p-mail', {autoAlpha:1, y:0}, {autoAlpha:0, y:40, duration:0.6, ease:'power3.out', onComplete : done}, "-=0.6")
-  })
-}
-
 
 function sliderOpacity() {
   const target = document.querySelector('.c-slider')
@@ -52,7 +32,6 @@ function sliderOpacity() {
 
 
 if (body.getAttribute('namespace') === 'home') {
-  modalStart()
   sliderOpacity()
 }
 
